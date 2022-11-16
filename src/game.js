1559313d2e657bef92db36d1e793875a0df30a35
Logan Raffkind <logan.raffkind@gmail.com>
2022-11-15 16:12:43 -0800

    Added basic introduction to Game class

--- src/game.js
+++ src/game.js
@@ -1,10 +1,27 @@
-import { Player } from "./player.js";
+import inquirer from "inquirer";
 
-export class Game {
+import Player from "./player.js";
+
+export default class Game {
 
     constructor() {
         let player = new Player();
         console.log("New game");
     }
+
+    init() {
+        this.introduction();
+    }
+
+    async introduction() {
+        await inquirer.prompt({
+            name: "introduction",
+            type: "input",
+            message: "What is your name?"
+        }).then(async (response) => {
+            console.log(response.introduction);
+        });
+    }
+
 
 }
