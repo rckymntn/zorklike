cdae660e88861f3479df5e8c41e8cc5d78c88aaa
Logan Raffkind <logan.raffkind@gmail.com>
2023-04-22 20:37:53 -0700

    Update util.ts

--- src/util.ts
+++ src/util.ts
@@ -1,10 +1,8 @@
+import Player from "./Player";
 
 export const writeToConsoleHistory = (toWrite: string): void => {
     const consoleHistory = document.getElementById("consoleHistory") as HTMLTextAreaElement;
     consoleHistory.value += `${toWrite}\n`;
-    if (toWrite.includes("clear")) {
-        consoleHistory.value = "";
-    }
     // Handle input
 }
 
@@ -12,6 +10,16 @@ export const readFromConsoleInput = (): void => {
     const consoleInput = document.getElementById("consoleInput") as HTMLInputElement;
     writeToConsoleHistory(consoleInput.value);
     consoleInput.value = "";
+}
+
+export const updateCharacterSheet = (player: Player): void => {
+    const characterSheet = document.getElementById("characterSheet") as HTMLTextAreaElement;
+    characterSheet.value = `${player.biography.name}\n${player.biography.race} ${player.biography.classType}\n\n${player.attributes.toString()}`
+}
+
+export const clearConsoleHistory = (): void => {
+    const consoleHistory = document.getElementById("consoleHistory") as HTMLTextAreaElement;
+    consoleHistory.value = "";
 }
 
 export const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
