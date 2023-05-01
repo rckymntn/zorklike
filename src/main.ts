a63b924e9425ecc3cd3476aa3fae680611dcb59f
Logan Raffkind <logan.raffkind@gmail.com>
2023-04-30 17:01:52 -0700

    Simplified main

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,7 @@
-import Goblin from "./Goblin";
-import Player from "./Player";
-import Room from "./Room";
-import ScreenManager from "./ScreenManager";
+import GameManager from "./GameManager";
 
 function main(): void {
-    const screenManager = new ScreenManager();
-
-    let startingRoom = new Room();
-    screenManager.writeToConsoleHistory(startingRoom.description);
-    let player = new Player();
-    let goblin = new Goblin();
-    startingRoom.actorList.push(player, goblin);
-    
-    player.attack(goblin);
-    goblin.attack(player);
-    player.talk(goblin);
-    goblin.talk(player);
-    
-    screenManager.writeToConsoleHistory("Greetings, adventurer!");
-    player.updateCharacterSheet();
-
+    const gameManager = new GameManager();
 }
 
-window.onload = main;
+window.onload = () => main();
